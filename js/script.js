a50f0d4e2ea4b03488063916c7b15605a0e14b53
anna <g.molybog@levi9.com>
2019-03-14 15:41:54 +0100

    rewrite timer using es6

--- js/script.js
+++ js/script.js
@@ -1,63 +1,63 @@
-let date = new Date(),
-    deadline = new Date(date.getTime() + (24 * 60 * 60 * 1000));
-console.log(date.getTime());
-function getTimeRemaining(deadline) {
-    let remainingTime = Date.parse(deadline) - Date.parse(new Date()),
+class Timer {
+    constructor(element) {
+        this.timer = element;
+    }
+    
+    _prepareTime(time) {
+        return (time < 10 ? "0" : "") + time;
+    }
+
+    _getTimeRemaining(deadline) {
+        let remainingTime = Date.parse(deadline) - Date.parse(new Date()),
         seconds = remainingTime > 0 ? Math.floor((remainingTime/1000) % 60) : 0,
         minutes = remainingTime > 0 ? Math.floor((remainingTime/1000/60) % 60) : 0,
         hours = remainingTime > 0 ? Math.floor((remainingTime/(1000*60*60))): 0;
 
-    return {
-        seconds: seconds,
-        minutes: minutes,
-        hours: hours,
-        total: remainingTime
+        return {
+            seconds: seconds,
+            minutes: minutes,
+            hours: hours,
+            total: remainingTime
+        }
     }
-}
 
-function getTimerElements(id) {
-    let timer = document.getElementById(id),
-        hours = timer.querySelector('.hours'),
-        minutes = timer.querySelector('.minutes'),
-        seconds = timer.querySelector('.seconds');
-
-    return {
-        seconds: seconds,            
-        minutes: minutes,
-        hours: hours
+    _getTimerElements() {
+        let hours = this.timer.querySelector('.hours'),
+            minutes = this.timer.querySelector('.minutes'),
+            seconds = this.timer.querySelector('.seconds');
+    
+        return {
+            seconds: seconds,            
+            minutes: minutes,
+            hours: hours
+        }
     }
-}
 
-function setTimeInElements(timerElements, remainingTime) {
-    timerElements.hours.textContent = prepareTime(remainingTime.hours);
-    timerElements.minutes.textContent = prepareTime(remainingTime.minutes);
-    timerElements.seconds.textContent = prepareTime(remainingTime.seconds);
-}
+    _setTimeInElements(timerElements, remainingTime) {
+        timerElements.hours.textContent = this._prepareTime(remainingTime.hours);
+        timerElements.minutes.textContent = this._prepareTime(remainingTime.minutes);
+        timerElements.seconds.textContent = this._prepareTime(remainingTime.seconds);
+    }
 
-function setDeadline(id, deadline) {
-    let timerElements = getTimerElements(id),
-        remainingTime = getTimeRemaining(deadline);
-
-    setTimeInElements(timerElements, remainingTime);
-}
-
-function setClock(id, deadline) {
-    let timerElements = getTimerElements(id),
-        timerInterval = setInterval(updateClock, 1000);
+    setClock(deadline) {
+        let timerElements = this._getTimerElements(),
+            timerInterval = setInterval(updateClock.bind(this), 1000);
         
-    function updateClock() {
-        let remainingTime = getTimeRemaining(deadline);
-        setTimeInElements(timerElements, remainingTime);
-
-        if (remainingTime.total <= 0) {
-            clearInterval(timerInterval, remainingTime)
+        this._setTimeInElements(timerElements, this._getTimeRemaining(deadline));
+            
+        function updateClock() {
+            let remainingTime = this._getTimeRemaining(deadline);
+            this._setTimeInElements(timerElements, remainingTime);
+    
+            if (remainingTime.total <= 0) {
+                clearInterval(timerInterval, remainingTime)
+            }
         }
     }
 }
 
-function prepareTime(time) {
-    return (time < 10 ? "0" : "") + time;
-}
+let date = new Date(),
+    deadline = new Date(date.getTime() + (24 * 60 * 60 * 1000)),
+    timer = new Timer(document.getElementById('timer'));
 
-setDeadline('timer', deadline);
-setClock('timer', deadline);
+timer.setClock(deadline);
